892d4de1dea09c053149eac4daec8cb8093cead3
legin8 <maynne1@student.op.ac.nz>
2022-09-04 17:32:33 +1200

    Added cpuPaddle class that is the child of paddle, with constructor

---
+++ CpuPaddle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pong_NEM
+{
+    public class CpuPaddle : Paddle
+    {
+        // Class Constructor
+        public CpuPaddle(Graphics graphics, Rectangle formRectangle) : base(graphics, formRectangle)
+        {
+        }
+
+
+    }
+}
