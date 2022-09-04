713eb32022d9b1d9ca5d2a47b0d2b888dd4704c2
legin8 <maynne1@student.op.ac.nz>
2022-09-04 17:25:28 +1200

    Fixed bug in abstract paddle class that lets player paddle go off the screen

--- Paddle.cs
+++ Paddle.cs
@@ -14,29 +14,31 @@ namespace Pong_NEM
         protected Brush brush;
         protected Graphics graphics;
         protected Rectangle formRectangle;
-        protected int paddlePositionY, paddleHeight, paddleSide;
+        protected int paddlePositionY, paddleSize, paddleSide;
         // Class Constructor
         public Paddle(Graphics graphics, Rectangle formRectangle)
         {
             this.graphics = graphics;
             this.formRectangle = formRectangle;
-            paddleHeight = 100;
+            paddleSize = 100;
         }
 
         public virtual void DrawPaddle()
         {
-            graphics.FillRectangle(brush, paddleSide, paddlePositionY, PADDLEWIDTH, paddleHeight);
+            graphics.FillRectangle(brush, paddleSide, paddlePositionY, PADDLEWIDTH, paddleSize);
         }
 
 
         public void PaddleYUp()
         {
             if (paddlePositionY > 0) paddlePositionY -= 10;
+            if (paddlePositionY < 0) paddlePositionY = formRectangle.Top;
         }
 
         public void PaddleYDown()
         {
-            if (paddlePositionY < formRectangle.Bottom - paddleHeight) paddlePositionY += 10;
+            if (paddlePositionY < formRectangle.Bottom - paddleSize) paddlePositionY += 10;
+            if (paddlePositionY > formRectangle.Bottom - paddleSize) paddlePositionY = formRectangle.Bottom - paddleSize;
         }
     }
 }
