03c0be8abb9cf5fd5b1a52bfb3d00c8137462931
legin8 <maynne1@student.op.ac.nz>
2022-09-10 12:48:40 +1200

    Added brush to scoreboard and put stuff in constructor, made gets for its Rectangle and brush

--- ScoreBoard.cs
+++ ScoreBoard.cs
@@ -4,16 +4,27 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Pong_NEM
 {
     public class ScoreBoard
     {
-        private Rectangle scoreBordRectangle, formRectangle;
+        // Class Variables
+        private Rectangle scoreBoardRectangle, formRectangle;
+        private int scoreBoardHeight;
+        private Brush brush = Brushes.DarkBlue;
 
+        // Gets
+        public Rectangle GetScoreBoardRectangle { get => scoreBoardRectangle; }
+        public Brush GetScoreBoardBrush { get => brush; }
+
+        // Class constructor
         public ScoreBoard(Rectangle formRectangle)
         {
             this.formRectangle = formRectangle;
+            scoreBoardHeight = formRectangle.Height / 4;
+            scoreBoardRectangle = new Rectangle(formRectangle.Left, formRectangle.Top, formRectangle.Width, scoreBoardHeight);
         }
 
     }
