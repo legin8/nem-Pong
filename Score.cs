54c8602dfa14d1b1a6b09334c4f93be44fe14d61
legin8 <maynne1@student.op.ac.nz>
2022-09-10 12:21:12 +1200

    Added abstract score class with variables, gets/ sets and constructor

---
+++ Score.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pong_NEM
+{
+    public abstract class Score
+    {
+        // Abstract Class
+        // Class Variables
+        protected int score = 0;
+        protected string name;
+
+        // Gets and sets
+        public int GetScore { get => score; set => score = value; }
+        public string Name { get => name; }
+
+        public Score(string name)
+        {
+            this.name = name;
+        }
+
+    }
+}
