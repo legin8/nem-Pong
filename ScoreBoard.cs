f45c83a184d0c263af4a5369e60ccef4d634f95f
Nigel Maynard (1000018419) <MAYNNE1@student.op.ac.nz>
2022-09-16 12:39:24 +1200

    refactored screboard and removed code

--- ScoreBoard.cs
+++ ScoreBoard.cs
@@ -11,20 +11,19 @@ namespace Pong_NEM
     public class ScoreBoard
     {
         // Class Variables
-        private Rectangle scoreBoardRectangle, formRectangle;
+        private Rectangle scoreBoardRectangle;
         private int scoreBoardHeight;
-        private Brush brush = Brushes.DarkBlue;
+        private Brush brush = Brushes.Blue;
 
         // Gets
-        public Rectangle GetScoreBoardRectangle { get => scoreBoardRectangle; }
-        public Brush GetScoreBoardBrush { get => brush; }
+        public Rectangle GetScoreBoardRectangle => scoreBoardRectangle;
+        public Brush GetScoreBoardBrush => brush;
         
 
         // Class constructor
         public ScoreBoard(Rectangle formRectangle)
         {
-            this.formRectangle = formRectangle;
-            scoreBoardHeight = formRectangle.Height / 4;
+            scoreBoardHeight = formRectangle.Height / 6;
             scoreBoardRectangle = new Rectangle(formRectangle.Left, formRectangle.Top, formRectangle.Width, scoreBoardHeight);
         }
 
