6e010113af83450119c6fd03b27265427b71c47c
Nigel Maynard (1000018419) <MAYNNE1@student.op.ac.nz>
2022-09-21 12:13:18 +1200

    Added Color class for random color

---
+++ Color.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pong_NEM
+{
+    internal class Color
+    {
+        private const int COLORRANGE = 255;
+
+
+        public Color()
+        {
+
+        }
+
+    }
+}
