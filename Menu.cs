566d5f0e8e1867909d0ae9138f4bbabe86db94c9
Nigel Maynard (1000018419) <MAYNNE1@student.op.ac.nz>
2022-09-23 08:00:42 +1200

    Added new const strings int the menu class and renamed the first font variable

--- Menu.cs
+++ Menu.cs
@@ -10,14 +10,15 @@ namespace Pong_NEM
 {
     public class Menu
     {
-        private const string MENUTEXT = "Press Esc Pause/Menu";
-        private Font font;
+        private const string MENUTEXT = "Press Esc Pause/Menu", NEWGAMETEXT = "Press N for a New Game", RESTARTGAMETEXT = "Press R to Restart",
+            RESUMETEXT = "Press Esc To Resume";
+        private Font font1;
 
         public string GetMenuText => MENUTEXT;
-        public Font GetFont => font;
+        public Font GetFont => font1;
         public Menu()
         {
-            font = new Font("Ariel", 14, FontStyle.Bold);
+            font1 = new Font("Ariel", 14, FontStyle.Bold);
         }
 
     }
